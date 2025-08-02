f418d5d8a6cc301bfde3b5cf127ce0b0bb27cd37
Rijndael <rijndael@baldy.ga>
2025-08-02 09:52:15 +0100

    added camera.. still need to fix code tho

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 @export var loop1: Loop
 
-func _input(event):
-	if event is InputEventMouseButton and event.is_released():
-		print("mouse button event at ", event.position)
-		loop1.position = event.position
-		loop1.do_punch_and_disappear()
+#func _input(event):
+	#if event is InputEventMouseButton and event.is_released():
+		#print("mouse button event at ", event.position)
+		#loop1.position = event.position
+		#loop1.do_punch_and_disappear()
