d33b7ebc19760c5627c20ca204ecc0e1d5bb1ef0
Rijndael <rijndael@baldy.ga>
2025-08-02 10:07:09 +0100

    doesn't work but it's clsoe

--- scripts/camera_2d.gd
+++ scripts/camera_2d.gd
@@ -1,29 +1,17 @@
 extends Camera2D
 
-signal spawnThing(v2: Vector2, mob: Node)
-signal damageEnemy(pos: Vector2, radius: int)
+signal spawn_loop(v2: Vector2)
 
 # Mouse state
 var _mouse_position = Vector2(0.0, 0.0)
-@export var mob_scene: PackedScene
-
-
 var mouse_position_log = []
 
 func _input(event):
-	var normalised_position = - self.get_screen_center_position() + event.position
-	
-	# Receives mouse motion
-	if event is InputEventMouseMotion:
-		if _mouse_position != event.relative:
-			print("CLICKING STOPP:", normalised_position)
-			_mouse_position = event.relative
-	
+	var normalised_position = event.global_position + self.position
+
 	# Receives mouse button input
 	if event is InputEventMouseButton:
 		match event.button_index:
 			MOUSE_BUTTON_RIGHT: # Only allows rotation if right click down
 				if event.pressed:
-					#var m = mob_scene.instantiate()
-					#spawnThing.emit(normalised_position, m)
-					pass
+					spawn_loop.emit(normalised_position)
--- scripts/player.gd
+++ scripts/player.gd
@@ -7,3 +7,9 @@ extends Node2D
 		#print("mouse button event at ", event.position)
 		#loop1.position = event.position
 		#loop1.do_punch_and_disappear()
+
+
+func _on_camera_2d_spawn_loop(v2: Vector2) -> void:
+	print("mouse button event at ", v2)
+	loop1.position = v2
+	loop1.do_punch_and_disappear()
