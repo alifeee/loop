314616fd1c3abf93fa2efbecd822c892f58deb85
alifeee <alifeee.web@outlook.com>
2025-08-02 16:50:46 +0100

    break out looping into functions

--- scripts/player.gd
+++ scripts/player.gd
@@ -26,13 +26,84 @@ func _init() -> void:
 	packed_loop_segment = preload("res://scenes/loop_segment.tscn")
 
 func do_loop_damage(position: Vector2, radius: float) -> void:
+	# check if each demon is in range and hit if it is
 	loop1.position = position
-	print(Globals.demons)
 	for demon in Globals.demons:
 		#print("distance: ", demon.global_position.distance_to(position))
 		if demon.global_position.distance_to(position) < radius:
 			#print("demon is hit !", demon)
 			demon.hit(DAMAGE_PERCENT)
+
+func pick_up_spell(pos):
+	# mouse down: spawn sprites and reset positions
+	is_held = true
+	mouse_positions = [pos]
+
+func drop_spell():
+	# mouse up or start menu/etc: drop all sprites
+	is_held = false
+	# delete sprite segments
+	for loop_segment in loop_segments:
+		loop_segment.queue_free()
+	mouse_positions = []
+	loop_segments = []
+
+func check_and_spawn_spell():
+	# check if loop can spawn (if loop was drawn well or badly)
+	#   and spawn if so
+	# work out centroid of loop
+	var centroid = Vector2(0,0)
+	for pos in mouse_positions:
+		centroid = centroid + (pos / len(mouse_positions))
+	# loop checks (SIZE / etc)
+	error = ""
+	##### RECTANGLE (not used) SIZING ####
+	#if false:
+		#var loop_rect = Rect2(mouse_positions[0], Vector2(0,0))
+		#for mouse_position in mouse_positions:
+			#loop_rect = loop_rect.expand(mouse_position)
+		#var rect_area = loop_rect.get_area()
+		#print("position:", loop_rect.position)
+		#print("end:", loop_rect.end)
+		#print("area:", rect_area)
+		#print("polgon area!")
+		#$position.position = loop_rect.position
+		#$end.position = loop_rect.end
+		#if rect_area > LOOP_MIN_AREA:
+			## move loop and trigger actions
+			#do_loop_damage(centroid, LOOP_RADIUS)
+		#else:
+			#error.emit("loop not big enough")
+	#### POLYGON SIZING ####
+	var polygon_area = abs(Globals.calc_polygon_area(mouse_positions))
+	if polygon_area < LOOP_MIN_AREA:
+		error = "loop not big enough"
+	#### START/END DISTANCE ####
+	var start_pos = mouse_positions[0]
+	var end_pos = mouse_positions[-1]
+	if start_pos.distance_to(end_pos) > LOOP_MAX_START_END_DISTANCE:
+		error = "start too far from end"
+	if error == "":
+		do_loop_damage(centroid, LOOP_RADIUS)
+	else:
+		error_debug.emit(error)
+
+func check_and_add_spell_point(pos):
+	# if point far from previous point, and if line not too long
+	#   add segment and carry on (wait for mouse up)
+	var last_position = mouse_positions[-1]
+	var this_position = pos
+	if this_position.distance_to(last_position) > LOOP_SPRITE_DISTANCE:
+		mouse_positions.append(this_position)
+		# add sprite at midpoint
+		var midpoint = last_position.lerp(this_position, 0.5)
+		var loopsprite: AnimatedSprite2D = packed_loop_segment.instantiate()
+		var num_frames = loopsprite.sprite_frames.get_frame_count("default")
+		loopsprite.frame = rng.randi_range(0, num_frames)
+		loopsprite.frame_progress = rng.randf_range(0, 1)
+		loopsprite.position = pos
+		loop_segments.append(loopsprite)
+		add_child(loopsprite)
 
 func _input(event):
 	# pause game
@@ -46,68 +117,14 @@ func _input(event):
 	# start looping!  
 	elif event is InputEventMouseButton and event.is_pressed() and event.is_action_pressed("Magic"):
 		if Globals.gamestate == Globals.GAMESTATES.PLAYING:
-			is_held = true
-			mouse_positions.append(event.position)
+			pick_up_spell(event.position)
 	# stop looping!
 	elif event is InputEventMouseButton and event.is_released() and is_held:
 		# only do this if unpaused
 		if Globals.gamestate == Globals.GAMESTATES.PLAYING:
-			# work out centroid of loop
-			var centroid = Vector2(0,0)
-			for pos in mouse_positions:
-				centroid = centroid + (pos / len(mouse_positions))
-			# loop checks
-			error = ""
-			#### RECTANGLE (not used) SIZING ####
-			if false:
-				var loop_rect = Rect2(mouse_positions[0], Vector2(0,0))
-				for mouse_position in mouse_positions:
-					loop_rect = loop_rect.expand(mouse_position)
-				var rect_area = loop_rect.get_area()
-				print("position:", loop_rect.position)
-				print("end:", loop_rect.end)
-				print("area:", rect_area)
-				print("polgon area!")
-				$position.position = loop_rect.position
-				$end.position = loop_rect.end
-				if rect_area > LOOP_MIN_AREA:
-					# move loop and trigger actions
-					do_loop_damage(centroid, LOOP_RADIUS)
-				else:
-					error.emit("loop not big enough")
-			#### POLYGON SIZING ####
-			var polygon_area = abs(Globals.calc_polygon_area(mouse_positions))
-			if polygon_area < LOOP_MIN_AREA:
-				error = "loop not big enough"
-			#### START/END DISTANCE ####
-			var start_pos = mouse_positions[0]
-			var end_pos = mouse_positions[-1]
-			if start_pos.distance_to(end_pos) > LOOP_MAX_START_END_DISTANCE:
-				error = "start too far from end"
-			if error == "":
-				do_loop_damage(centroid, LOOP_RADIUS)
-			else:
-				error_debug.emit(error)
+			check_and_spawn_spell()
 		# do this whatever the weather
-		is_held = false
-		# delete sprite segments
-		for loop_segment in loop_segments:
-			loop_segment.queue_free()
-		mouse_positions = []
-		loop_segments = []
+		drop_spell()
 	# continue looping !
 	elif event is InputEventMouseMotion and is_held:
-		#print(event)
-		var last_position = mouse_positions[-1]
-		var this_position = event.position
-		if this_position.distance_to(last_position) > LOOP_SPRITE_DISTANCE:
-			mouse_positions.append(this_position)
-			# add sprite at midpoint
-			var midpoint = last_position.lerp(this_position, 0.5)
-			var loopsprite: AnimatedSprite2D = packed_loop_segment.instantiate()
-			var num_frames = loopsprite.sprite_frames.get_frame_count("default")
-			loopsprite.frame = rng.randi_range(0, num_frames)
-			loopsprite.frame_progress = rng.randf_range(0, 1)
-			loopsprite.position = event.position
-			loop_segments.append(loopsprite)
-			add_child(loopsprite)
+		check_and_add_spell_point(event.position)
