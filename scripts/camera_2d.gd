726192864878695392952c41ea0f7eb7bc42d89a
Rijndael <rijndael@baldy.ga>
2025-08-02 10:08:23 +0100

    reverted changes

--- scripts/camera_2d.gd
+++ scripts/camera_2d.gd
@@ -1,17 +1,1 @@
 extends Camera2D
-
-signal spawn_loop(v2: Vector2)
-
-# Mouse state
-var _mouse_position = Vector2(0.0, 0.0)
-var mouse_position_log = []
-
-func _input(event):
-	var normalised_position = event.global_position + self.position
-
-	# Receives mouse button input
-	if event is InputEventMouseButton:
-		match event.button_index:
-			MOUSE_BUTTON_RIGHT: # Only allows rotation if right click down
-				if event.pressed:
-					spawn_loop.emit(normalised_position)
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,14 +2,8 @@ extends Node2D
 
 @export var loop1: Loop
 
-#func _input(event):
-	#if event is InputEventMouseButton and event.is_released():
-		#print("mouse button event at ", event.position)
-		#loop1.position = event.position
-		#loop1.do_punch_and_disappear()
-
-
-func _on_camera_2d_spawn_loop(v2: Vector2) -> void:
-	print("mouse button event at ", v2)
-	loop1.position = v2
-	loop1.do_punch_and_disappear()
+func _input(event):
+	if event is InputEventMouseButton and event.is_released():
+		print("mouse button event at ", event.position)
+		loop1.position = event.position
+		loop1.do_punch_and_disappear()
