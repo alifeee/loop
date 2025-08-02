7ff8ed22250f4fb3208613295bc626b4b0139fbc
Rijndael <rijndael@baldy.ga>
2025-08-02 10:25:22 +0100

    removed camera, now tracks mouse movement when pressed

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,8 +2,29 @@ extends Node2D
 
 @export var loop1: Loop
 
+
+var mouse_positions = []
+var is_held = true
+
+
 func _input(event):
+	if event is InputEventMouseButton and event.is_pressed():
+		is_held = true
+
 	if event is InputEventMouseButton and event.is_released():
+		is_held = false
+		
+		print(mouse_positions)
 		print("mouse button event at ", event.position)
 		loop1.position = event.position
 		loop1.do_punch_and_disappear()
+		
+	if is_held:
+		print("ee")
+		
+		# calculate distance difference (refactor this)
+		if len(mouse_positions) == 0:
+			mouse_positions.append(event.position)
+		else: 
+			if len(mouse_positions) and event.position.distance_to(mouse_positions[-1]) > 0.5:
+				mouse_positions.append(event.position)
