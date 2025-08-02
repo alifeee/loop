8396612b5b9a366b01dc66619c17fbb4219bcee9
Rijndael <rijndael@baldy.ga>
2025-08-02 17:28:33 +0100

    making drop object

--- scripts/drop.gd
+++ scripts/drop.gd
@@ -1,1 +1,21 @@
-extends Sprite2D
+extends AnimatableBody2D
+
+@export var initial_varience: float = 5000
+@export var velocity_dropoff: float = 0.999
+@export var minimal_movement: float = 1e-2
+
+# var target_loc: Vector2
+
+# func _physics_process(delta: float) -> void:
+
+var drop_tween: Tween
+
+func _on_ready() -> void:
+	print("Ready!!")
+	var angle = randf() * 2 * PI
+	var target_loc = Vector2(
+		sin(angle),
+		cos(angle),
+	) * initial_varience
+	
+	
