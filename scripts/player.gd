4d46633b9dc914205c9be8660f879f5b0b1a1ecb
Rijndael <rijndael@baldy.ga>
2025-08-02 12:02:16 +0100

    loops now close

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,9 @@ extends Node2D
 
 @export var loop1: Loop
 @export var LOOP_SPRITE_DISTANCE: float = 5
+@export var LOOP_FIRST_SEGMENT_MAGNETISM = 20
 @export var LOOP_RADIUS: float = 50
+@export var LOOP_MIN_SIZE = 60
 @export var LOOP_THICCNESS = 2
 @export var DAMAGE_PERCENT: float = 50
 
@@ -22,26 +24,47 @@ func do_loop_damage(position: Vector2, radius: float) -> void:
 		if demon.global_position.distance_to(position) < radius:
 			#print("demon is hit !", demon)
 			demon.hit(DAMAGE_PERCENT)
+			
+func add_loop_segment(pos1: Vector2, pos2: Vector2):
+	var dist = pos1.distance_to(pos2)
+	var midpoint = pos1.move_toward(pos2, dist / 2)
+	
+	var loop: Sprite2D = packed_loop_segment.instantiate()
+	loop.position = midpoint
+	loop.rotation = pos1.angle_to_point(pos2) + PI / 2
+	loop.apply_scale(Vector2(LOOP_THICCNESS, dist / 2))
+	
+	loop_segments.append(loop)
+	add_child(loop)
 
-func add_mouse_position(v2: Vector2):
+
+func add_mouse_position(v2: Vector2) -> void:
 	mouse_positions.append(v2)
 	
 	if len(mouse_positions) > 2:
-		var pos1 = mouse_positions[-1]
-		var pos2 = mouse_positions[-2]
-		var dist = pos1.distance_to(pos2)
-		var midpoint = pos1.move_toward(pos2, dist / 2)
+		# add segment to current loop
+		add_loop_segment(mouse_positions[-1], mouse_positions[-2])
 		
-		var loop: Sprite2D = packed_loop_segment.instantiate()
-		loop.position = midpoint
-		loop.rotation = pos1.angle_to_point(pos2) + PI / 2
-		loop.apply_scale(Vector2(LOOP_THICCNESS, dist / 2))
-		
-		loop_segments.append(loop)
-		add_child(loop)
-		
+		# check for loop closing
+		if mouse_positions[-1].distance_to(mouse_positions[0]) < LOOP_FIRST_SEGMENT_MAGNETISM:
+			var mx = Vector2(-INF, -INF) # we calculate the max and min xy
+			var mi = Vector2(INF, INF)
+			for pos in mouse_positions:
+				mx = mx.max(pos)
+				mi = mi.min(pos)
+				
+			var dist = mi.distance_to(mx)
 
-func finish_mouse_loop(last_mouse_pos: Vector2):
+			print(mx, mi, dist)
+			
+			if dist > LOOP_MIN_SIZE:
+				add_loop_segment(mouse_positions[-1], mouse_positions[0])
+				finish_mouse_loop()
+
+
+func finish_mouse_loop():
+	is_held = false
+
 	# work out centroid of loop
 	# There needs to be some kind of weighting by distance
 	var tot_vector = Vector2(0,0)
@@ -67,8 +90,7 @@ func _input(event):
 
 	# stop looping!
 	if event is InputEventMouseButton and event.is_released():
-		is_held = false
-		finish_mouse_loop(event.position)
+		finish_mouse_loop()
 
 	# calculate distance difference
 	if is_held and (len(mouse_positions) == 0 or (len(mouse_positions) and event.position.distance_to(mouse_positions[-1]) > LOOP_SPRITE_DISTANCE)):
