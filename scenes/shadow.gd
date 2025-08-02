0aa3956679a928ba31630352ac81be3c499ba0a2
flxli227 <flxli227@gmail.com>
2025-08-03 00:14:04 +0100

    Add some more pulsing element backgrounds

--- scenes/shadow.gd
+++ scenes/shadow.gd
@@ -1,6 +1,15 @@
 extends Sprite2D
 
+# Size Flikering
+@export var keep_aspect_ratio: bool
+@export var base_size: float
 @export var flicker_amplitude: float
+
+# Alpha Flikering
+@export var alpha_flickering: bool
+@export var base_alpha: float
+@export var alpha_amplitude: float
+
 @export var flicker_time_min: float
 @export var flicker_time_max: float
 
@@ -15,13 +24,32 @@ func _ready() -> void:
 	
 func randomly_make_shadow_bigger_and_smaller():
 	var tween = get_tree().create_tween()
+	
+	var tween_time = randf_range(flicker_time_min, flicker_time_max)
+	
+	var random_vector2
+	if keep_aspect_ratio:
+		var random_value = randf_range(base_size - flicker_amplitude, base_size + flicker_amplitude)
+		random_vector2 = Vector2(
+			random_value,
+			random_value,
+		)
+	else:
+		random_vector2 = Vector2(
+			randf_range(base_size - flicker_amplitude, base_size + flicker_amplitude),
+			randf_range(base_size - flicker_amplitude, base_size + flicker_amplitude),
+		)
 	tween.tween_property(
 		self, 
 		"scale", 
-		Vector2(
-			randf_range(1, 1 + flicker_amplitude),
-			randf_range(1, 1 + flicker_amplitude),
-		), 
-		randf_range(flicker_time_min, flicker_time_max)
+		random_vector2, 
+		tween_time
 	)
+	if alpha_flickering:
+		tween.parallel().tween_property(
+			self, 
+			"self_modulate:a", 
+			randf_range(base_alpha - alpha_amplitude, base_alpha + alpha_amplitude), 
+			tween_time
+		)
 	tween.tween_callback(randomly_make_shadow_bigger_and_smaller)
