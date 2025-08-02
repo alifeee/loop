9172bc8b9583a3ea21d27d9ed3361848107d7f7c
alifeee <alifeee.web@outlook.com>
2025-08-02 11:04:06 +0100

    try implement looping from branch

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,51 @@
 extends Node2D
 
 @export var loop1: Loop
+@export var LOOP_SPRITE_DISTANCE: float = 5
+
+var mouse_positions: Array[Vector2] = []
+var loop_segments: Array[Sprite2D] = []
+var is_held = false
+var packed_loop_segment: PackedScene
+
+func _init() -> void:
+	packed_loop_segment = preload("res://scenes/loop_segment.tscn")
+
+func add_mouse_position(v2: Vector2):
+	mouse_positions.append(v2)
+	var loop: Sprite2D = packed_loop_segment.instantiate()
+	loop.position = v2
+	loop_segments.append(loop)
+	add_child(loop)
 
 func _input(event):
+	# start looping!
+	if event is InputEventMouseButton and event.is_pressed():
+		is_held = true
+	# stop looping!
 	if event is InputEventMouseButton and event.is_released():
-		#print("mouse button event at ", event.position)
-		loop1.position = event.position
+		is_held = false
+		# work out centroid of loop
+		var tot_vector = Vector2(0,0)
+		for pos in mouse_positions:
+			tot_vector = tot_vector + pos
+		var avg_vector = Vector2(
+			tot_vector.x / len(mouse_positions),
+			tot_vector.y / len(mouse_positions)
+		)
+		mouse_positions = []
+		# move loop and trigger actions
+		loop1.position = avg_vector
 		loop1.do_punch_and_disappear()
+		# delete sprite segments
+		for loop_segment in loop_segments:
+			loop_segment.queue_free()
+		loop_segments = []
+	# continue looping !
+	if is_held:
+		# calculate distance difference (refactor this)
+		if len(mouse_positions) == 0:
+			add_mouse_position(event.position)
+		else: 
+			if len(mouse_positions) and event.position.distance_to(mouse_positions[-1]) > LOOP_SPRITE_DISTANCE:
+				add_mouse_position(event.position)
