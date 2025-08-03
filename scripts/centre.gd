e579f1c86d3d7c94617b97f06d8f92d846e574a7
alifeee <alifeee.web@outlook.com>
2025-08-03 15:56:43 +0100

    add tween for each wizard

--- scripts/centre.gd
+++ scripts/centre.gd
@@ -3,7 +3,10 @@ extends Area2D
 @export var wizards_array: Array[AnimatedSprite2D]
 @export var hats_array: Array[Sprite2D]
 
-var deathtween
+var deathtween1: Tween
+var deathtween2: Tween
+var deathtween3: Tween
+var deathtweens = [deathtween1, deathtween2, deathtween3]
 
 func _ready() -> void:
 	Globals.player_hit.connect(animate_wizard_death)
@@ -15,14 +18,16 @@ func reset() -> void:
 
 	for wizard in wizards_array:
 		wizard.modulate.a = 1
-	if deathtween:
-		deathtween.kill()
+	for t in deathtweens:
+		if t:
+			t.kill()
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Demon:
 		body.reach_middle()
 
 func animate_wizard_death(wizard_number: int):
+	var deathtween = deathtweens[wizard_number]
 	if deathtween:
 		deathtween.kill()
 
