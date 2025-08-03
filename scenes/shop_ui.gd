29abf678d9c9b0f1e1b323a2e9c0741e7be29ed3
flxli227 <flxli227@gmail.com>
2025-08-03 12:48:43 +0100

    Show Mote count in shop

--- scenes/shop_ui.gd
+++ scenes/shop_ui.gd
@@ -1,8 +1,17 @@
 extends Control
 
+@export var mote_count_label: Label
+
 func _ready() -> void:
 	Globals.button_pressed.connect(handle_shop_button_pressed)
+	
+func _process(delta: float) -> void:
+	update_mote_count(Globals.motes)
 	
 func handle_shop_button_pressed(button_name_id: String):
 	if button_name_id == "hats":
 		Globals.purchase_hat.emit()
+
+func update_mote_count(number_of_motes: int):
+	mote_count_label.text = str(number_of_motes)
+	
