c54816450b9eff6c5c8ad727dc75a6782dc1bf77
flxli227 <flxli227@gmail.com>
2025-08-03 13:22:58 +0100

    Add level bar on the Hat button in shop

--- scenes/shop_ui.gd
+++ scenes/shop_ui.gd
@@ -1,12 +1,15 @@
 extends Control
 
 @export var mote_count_label: Label
+@export var hat_button: Node
+
 
 func _ready() -> void:
 	Globals.button_pressed.connect(handle_shop_button_pressed)
 	
 func _process(delta: float) -> void:
 	update_mote_count(Globals.motes)
+	hat_button.update_current_level(Globals.hats_owned)
 	
 func handle_shop_button_pressed(button_name_id: String):
 	if button_name_id == "hats":
@@ -15,3 +18,5 @@ func handle_shop_button_pressed(button_name_id: String):
 func update_mote_count(number_of_motes: int):
 	mote_count_label.text = str(number_of_motes)
 	
+#func initialise_buttons():
+	#hat_button.update_current_level(Globals.hats_owned)
