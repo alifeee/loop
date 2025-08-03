bc99f1fc1fc03d36292c33bb96b98c9070616464
alifeee <alifeee.web@outlook.com>
2025-08-03 15:41:45 +0100

    fix wizard disappear on reset

--- scripts/centre.gd
+++ scripts/centre.gd
@@ -3,6 +3,8 @@ extends Area2D
 @export var wizards_array: Array[AnimatedSprite2D]
 @export var hats_array: Array[Sprite2D]
 
+var deathtween
+
 func _ready() -> void:
 	Globals.player_hit.connect(animate_wizard_death)
 	Globals.reset_game.connect(reset)
@@ -11,16 +13,20 @@ func _ready() -> void:
 func reset() -> void:
 	for wizard in wizards_array:
 		wizard.modulate.a = 1
+	if deathtween:
+		deathtween.kill()
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Demon:
 		body.reach_middle()
 
 func animate_wizard_death(wizard_number: int):
-	var tween = get_tree().create_tween()
-	tween.tween_property(wizards_array[wizard_number], "modulate:a", 0, 0.1)
-	tween.tween_property(wizards_array[wizard_number], "modulate:a", 1, 0.1)
-	tween.tween_property(wizards_array[wizard_number], "modulate:a", 0, 1)
+	if deathtween:
+		deathtween.kill()
+	deathtween = get_tree().create_tween()
+	deathtween.tween_property(wizards_array[wizard_number], "modulate:a", 0, 0.1)
+	deathtween.tween_property(wizards_array[wizard_number], "modulate:a", 1, 0.1)
+	deathtween.tween_property(wizards_array[wizard_number], "modulate:a", 0, 1)
 	print("Someone got hit... :( " + str(wizard_number))
 
 func buyhat():
