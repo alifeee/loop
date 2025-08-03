34dae273414ba395e5b3138bba69ad35f787bcd4
Freya <freya.connor@softwire.com>
2025-08-03 11:45:38 +0100

    feat: win screen

--- scripts/animated_sprite.gd
+++ scripts/animated_sprite.gd
@@ -3,4 +3,11 @@ extends AnimatedSprite2D
 func _ready() -> void:
 	Globals.pause_game.connect(pause)
 	Globals.resume_game.connect(play)
-	Globals.end_game.connect(pause)
+	Globals.end_game.connect(_win)
+
+func _win ():
+	play("summon_portal")
+	await get_tree().create_timer(1.5).timeout
+	play("open_portal")
+	await get_tree().create_timer(1.0).timeout
+	play("portal")	
